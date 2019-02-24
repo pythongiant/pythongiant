dc8771e09d3566448a2275154737f074a23292b3
srihari unnikrishnan <srihari.unnikrishnan@gmail.com>
2019-02-24 16:50:09 +0530

    make cool animatios

--- script.js
+++ script.js
@@ -7,4 +7,30 @@ $(document).ready(function(){
             
         }); 
     })
+
+
+var waypoint = new Waypoint({
+    element: document.getElementById('buttons'),
+    handler: function() {
+        console.log("yolo")
+        anime({
+            targets:"#python-graph",
+            width:"70%"
+        })
+        anime({
+            targets:"#kotlin-graph",
+            width:"60%"
+        })
+        anime({
+            targets:"#js-graph",
+            width:"50%"
+        })
+        anime({
+          targets:"#linux-graph",
+          width:"55%"
+        })   
+    }
+  })
+  
+  
 });
