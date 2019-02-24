aee73a32a8ef098cbd80c23707219d89dffacbcc
srihari unnikrishnan <srihari.unnikrishnan@gmail.com>
2019-02-24 14:13:01 +0530

    did some changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $(".title").fadeIn(500);
+   $(".title").fadeIn(500);
+   $("#music > i").hover(function(){
+        anime({
+            targets: '#music > i',
+            rotate:{value:'90deg',delay:300},
+            
+        }); 
+    })
 });
