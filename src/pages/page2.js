1e405425149a2dc21ae18a0fd8d47ac602ad7537
iampoppyxx <srihari.unnikrishnan@gmail.com>
2022-03-24 16:56:24 +0530

    init

---
+++ src/pages/page2.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./page2.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+export default class Page1 extends Component {
+  constructor() {
+    super();
+    this.socials = [
+      {
+        key: 0,
+        icon: <i className="fa-brands fa-figma"></i>,
+        link: "https://www.figma.com/",
+      },
+      {
+        key: 1,
+        icon:<i className="fa-brands fa-node-js"></i>,
+        link: "mailto:srihari.unnikrishnan@gmail.com",
+      },
+      {
+        key: 2,
+        icon: <i className="fa-brands fa-react"></i>,
+        link: "https://www.linkedin.com/in/srihari-unnikrishnan-b567a7145/",
+      },
+
+      { key: 4, icon: <i className="fa-brands fa-python"></i>, link: "" },
+      { key: 5, icon: <i className="fa-brands fa-html5"></i>, link: "" },
+      { key: 6, icon: <i className="fa-brands fa-sass"></i>, link: "" },
+    ];
+  }
+  render() {
+    return (
+      <div className="skills">
+        <h2>Skills</h2>
+        <p>
+          I mainly work with JavaScript and Python Web and Native Frameworks, my
+          area of speciality is building small and medium scale web apps.{" "}
+        </p>
+        <div className="icons">
+          {this.socials.map((item) => {
+            return (
+              <div>
+                <a href={item.link}>{item.icon}</a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
