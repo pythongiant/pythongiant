b511e918ae51764629b91a8f7ebd59e54d6308a4
iampoppyxx <srihari.unnikrishnan@gmail.com>
2022-07-08 07:12:11 +0530

    fix issues

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component, useEffect } from "react";
 import "./navbar.scss";
 import globals from "../../_globals.scss";
@@ -14,49 +13,25 @@ export default class Navbar extends Component {
     const executeScroll2 = () => {this.props.refs[1].current.executeScroll()};
     const executeScroll3 = () => {this.props.refs[2].current.executeScroll()};
 
-=======
-import React, { Component } from "react";
-import "./navbar.scss";
-import globals from "../../_globals.scss";
-
-export default class Navbar extends Component {
-    constructor(){
-        super();
-        const ref = React.createRef();  
-    }
-  render() {
->>>>>>> a6832cea78bbf91d9698cf199b307a0f490cdcda
     return (
       
         <div className="nav">
           <li>
-<<<<<<< HEAD
             <div onClick={executeScroll1}>
-=======
-            <div>
->>>>>>> a6832cea78bbf91d9698cf199b307a0f490cdcda
               <div className="navT"><p>about me</p></div>
               <div className="circle"></div>
             </div>
           </li>
 
           <li>
-<<<<<<< HEAD
             <div onClick={executeScroll2}>
-=======
-            <div>
->>>>>>> a6832cea78bbf91d9698cf199b307a0f490cdcda
               <div className="navT"><p>skills</p></div>
               <div className="circle"></div>
             </div>
           </li>
 
           <li>
-<<<<<<< HEAD
             <div onClick={executeScroll3}>
-=======
-            <div>
->>>>>>> a6832cea78bbf91d9698cf199b307a0f490cdcda
               <div className="navT"><p>projects</p></div>
               <div className="circle"></div>
             </div>
